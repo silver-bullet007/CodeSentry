1b1ba65262c49071d65fdbce81be6cb0fa198460
Lokesh <lokeshsundarav@gmail.com>
2026-08-24 22:27:20 +0530

    starting a java project CODESENTRY with spring AI

---
+++ src/test/java/com/codesentry/codesentry/CodesentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesentry.codesentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
